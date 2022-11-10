092cd69fc35ff5b0c78408c01b575beb6b37260e
Serg <108056276+username2022UA@users.noreply.github.com>
2022-11-10 17:06:35 +0200

    new

---
+++ JavaScript/Project-JS-1/js/script.js
@@ -0,0 +1,14 @@
+console.log('35' + - "22");
+
+console.log("35" * "22");
+
+//console.log("558" > 22++);
+
+let userCounter = 0;
+let newUser = userCounter++;
+console.log(newUser);
+
+console.log(!false && 11 || 18 && !'');
+
+let name = 0;
+console.log(name ?? "без имени"); 
