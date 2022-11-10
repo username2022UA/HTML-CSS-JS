63c237bf47e04d0507f2b8210d53c5c61a283d36
Serg <108056276+username2022UA@users.noreply.github.com>
2022-11-10 17:50:31 +0200

    new

--- JavaScript/Project-JS-1/js/script.js
+++ JavaScript/Project-JS-1/js/script.js
@@ -12,3 +12,37 @@ console.log(!false && 11 || 18 && !'');
 
 let name = 0;
 console.log(name ?? "без имени"); 
+
+let message = "Hello";
+/*if (5 < 10) {
+	messageEnd = ", Вася!";
+}
+else {
+	messageEnd = ", Вика!"
+}
+message += messageEnd;
+console.log(message);*/
+
+let messageEnd = 5 > 10 ? ", Вася!" : ", Вика!";
+message += messageEnd;
+console.log(message);
+
+if (1 === "1") {
+	console.log("1");}
+else { console.log("0");}
+
+if (5 == "5") {
+	console.log("1");}
+else {
+	console.log("0");
+}
+
+let message1 = (92 > '11' && 58 < 100) ? "1" : "0";
+console.log(message1);
+
+if (0) {
+	console.log("0");
+}
+else if (" "){
+	console.log("1");
+}
