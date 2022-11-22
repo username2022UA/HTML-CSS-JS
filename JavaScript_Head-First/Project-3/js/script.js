517855b5a2b5465dd1f07a1689ab4f49c7673e00
Serg <108056276+username2022UA@users.noreply.github.com>
2022-11-22 16:23:20 +0200

    new

---
+++ JavaScript_Head-First/Project-3/js/script.js
@@ -0,0 +1,40 @@
+var temp = 10;
+var tempFareng = (9 / 5) * temp + 32;
+console.log(tempFareng);
+
+//==================================
+
+//var color = "pink";
+var color = "orange";
+var logic = color == "orange";
+console.log(logic);
+
+//==================================
+
+var name = "Marta";
+var nameText = name + ", " + "you've won!";
+console.log(nameText);
+
+//==================================
+
+var yourLevel = 7;
+var result = yourLevel >= 5;
+console.log(result);
+
+//==================================
+
+var level = 5, points = 30000, bonus = 3300;
+var calcul = (level * points) + bonus;
+console.log(calcul);
+
+//==================================
+
+var color = "pink";
+var result = color != "orange";
+console.log(result);
+
+//==================================
+
+var sum = 1000 + "108";
+console.log(sum);
+console.log(typeof sum);
