96e5a235657ef40a513902dd3b26a0009ede7f98
Serg <108056276+username2022UA@users.noreply.github.com>
2022-11-22 16:47:16 +0200

    new

---
+++ JavaScript_Head-First/while/js/script.js
@@ -0,0 +1,21 @@
+/*
+var scoops = 5;
+while (scoops > 0) {
+	document.write("Another scoop!<br>");
+	scoops = scoops - 1;
+}
+document.write("Life without ice cream isn't the same");
+*/
+
+//================================
+
+/*
+var name = "Joe";
+var i = 0;
+while (i < 2) {
+	document.write("Happy Birthday to you.<br>");
+	i = i + 1;
+}
+document.write("Happy Birthday dear " + name + ",<br>");
+document.write("Happy Birthday to you.<br>");
+*/
