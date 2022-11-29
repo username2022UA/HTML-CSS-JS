ea7b7b80f64a4917989aac80ed92ccba8d08a429
Serg <108056276+username2022UA@users.noreply.github.com>
2022-11-29 20:50:20 +0200

    new

---
+++ JavaScript_Head-First/arrays/js/script.js
@@ -0,0 +1,19 @@
+var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+var last = products.length - 1;
+console.log(products[last]);
+
+// Генератор случайных фраз
+function makePhrases() {
+	var words1 = ["24/7", "multi-tier", "30,000 foot", "B-to-B", "win-win"];
+	var words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
+	var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
+	var rand1 = Math.floor(Math.random() * words1.length);
+	var rand2 = Math.floor(Math.random() * words2.length);
+	var rand3 = Math.floor(Math.random() * words3.length);
+	var phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
+	alert(phrase);
+}
+makePhrases();
+
+//========================================
+
