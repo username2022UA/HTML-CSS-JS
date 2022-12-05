304025aaf8a7435e8eecf555f454220f01a8c5f8
Serg <108056276+username2022UA@users.noreply.github.com>
2022-12-05 22:57:33 +0200

    new

--- JavaScript/arrays/js/script.js
+++ JavaScript/arrays/js/script.js
@@ -239,12 +239,12 @@ let reduceValue3 = arrPrev.reduce(function(previousValue, item, index, array) {
 
 let arrNambers = [-1, 2, 3, 7, 10];
 
-function arrNamberSumm(arr, namber) {
+function arrNamberSumm(arr, namber) { // O(n²)
 	for (let i = 0; i < arr.length; i++) {
 		for (let n = 0 + i; n < arr.length; n++) {
 			if ((arr[i] + arr[n] == namber) && (i != n)) {
-				let arrSumm = new Array(arr[i], arr[n]);
-				console.log(arrSumm);
+				let arrTwoNamber = new Array(arr[i], arr[n]);
+				console.log(arrTwoNamber);
 				return;
 			}
 		}
@@ -252,14 +252,56 @@ function arrNamberSumm(arr, namber) {
 }
 arrNamberSumm(arrNambers, 9);
 
-function arrNamberSumm2(arr, namber) {
+function arrNamberSumm2(arr, namber) { // O(n²)?
+	let x;
 	for (let i = 0; i < arr.length; i++) {
-		let x = namber - arr[i];
+		x = namber - arr[i];
 		if (arr.includes(x, i + 1)) {
-			let arrSumm = new Array(arr[i], arr[arr.indexOf(x, i + 1)]);
-			console.log(arrSumm);
+			let y = arr[arr.indexOf(x, i + 1)];
+			let arrTwoNamber = new Array(arr[i], y);
+			console.log(arrTwoNamber);
 			return;
 		}
 	}
 }
 arrNamberSumm2(arrNambers, 9);
+
+function arrNamberSumm3(arr, namber) { // O(n)
+	let x;
+	let filter = [];
+	for (let i = 0; i < arr.length; i++) {
+		x = namber - arr[i];
+		if (filter.includes(x, 0)) {
+			let arrTwoNamber = new Array(x, arr[i]);
+			console.log(arrTwoNamber);
+			return;
+		} else {
+			filter.push(arr[i]);
+		}
+	}
+}
+arrNamberSumm3(arrNambers, 9)
+
+let arrNambers2 = [2, 3, 7, 8, 9, 17, 22, 25, 29];
+
+// Бинарный поиск  O(n log n)
+function arrNamberSumm4(arr, namber) {
+	for (let i = 0; i < arr.length; i++) {
+		let x = namber - arr[i];
+		let arrNew = arr.slice(i + 1);
+		while (arrNew.length >= 1) {
+			let center = Math.floor(arrNew.length / 2);
+			if (x == arrNew[center]) {
+				let arrTwoNamber = new Array(arr[i], arrNew[center]);
+				console.log("Пара чисел: " + arrTwoNamber);
+				return;
+			} else if (x < arrNew[center]) {
+				arrNew = arrNew.slice(0, center);
+			} else if (x > arr[center]) {
+				arrNew = arrNew.slice(center + 1);
+			}
+		}
+	}
+	console.log("Нет такой пары чисел!");
+}
+arrNamberSumm4(arrNambers2, 11);
