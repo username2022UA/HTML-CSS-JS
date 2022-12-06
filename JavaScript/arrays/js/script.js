ec63ff0284dc648dd13710f916aac7a4054f9971
Serg <108056276+username2022UA@users.noreply.github.com>
2022-12-06 22:00:23 +0200

    new

--- JavaScript/arrays/js/script.js
+++ JavaScript/arrays/js/script.js
@@ -111,7 +111,7 @@ console.log(arrSort.sort());
 
 // Функция сортировка
 let arrSort2 = [8, 5, 1, 6, 2, 4];
-function sortNambers (a, b) {
+function sortNambers(a, b) {
 	console.log("Сравниваем ${a} и ${b}");
 	if (a > b) return 1;
 	if (a == b) return 0;
@@ -125,7 +125,7 @@ console.log(arrSort2.sort((a, b) => a - b));
 // ============ Сортировка массивов ================= //
 
 // map
-let resultSort = arrSort.map(function(item, index, array) {
+let resultSort = arrSort.map(function (item, index, array) {
 	return item[0];
 });
 console.log(resultSort);
@@ -161,7 +161,7 @@ arrName.forEach(function (item, index, array) {
 	console.log(`${item} находится на ${index} позиции в ${array}`);
 });
 
-arrName.forEach((item, index, array)  => {
+arrName.forEach((item, index, array) => {
 	console.log(`${item} находится на ${index} позиции в ${array}`);
 });
 
@@ -172,7 +172,7 @@ function show(item) {
 
 // метод перебора reduce/reduceRight
 let arrNamber = [1, 2, 3, 4];
-let reduceNamber = arrNamber.reduce(function(previousValue, item, index, array) {
+let reduceNamber = arrNamber.reduce(function (previousValue, item, index, array) {
 	return item + previousValue;
 }, 0);
 console.log(reduceNamber);
@@ -231,7 +231,7 @@ console.log(strinArr);
 
 // Задача №5.
 let arrPrev = [9, 2, 8];
-let reduceValue3 = arrPrev.reduce(function(previousValue, item, index, array) {
+let reduceValue3 = arrPrev.reduce(function (previousValue, item, index, array) {
 	console.log(previousValue);
 });
 
@@ -240,6 +240,7 @@ let reduceValue3 = arrPrev.reduce(function(previousValue, item, index, array) {
 let arrNambers = [-1, 2, 3, 7, 10];
 
 function arrNamberSumm(arr, namber) { // O(n²)
+	if (arr && namber) {
 	for (let i = 0; i < arr.length; i++) {
 		for (let n = 0 + i; n < arr.length; n++) {
 			if ((arr[i] + arr[n] == namber) && (i != n)) {
@@ -249,59 +250,104 @@ function arrNamberSumm(arr, namber) { // O(n²)
 			}
 		}
 	}
+	} else {
+		console.log("Недостаточно аргументов!");
+	}
 }
 arrNamberSumm(arrNambers, 9);
 
+//==============================================================
+
 function arrNamberSumm2(arr, namber) { // O(n²)?
-	let x;
-	for (let i = 0; i < arr.length; i++) {
-		x = namber - arr[i];
-		if (arr.includes(x, i + 1)) {
-			let y = arr[arr.indexOf(x, i + 1)];
-			let arrTwoNamber = new Array(arr[i], y);
-			console.log(arrTwoNamber);
-			return;
+	if (arr && namber) {
+		let x;
+		for (let i = 0; i < arr.length; i++) {
+			x = namber - arr[i];
+			if (arr.includes(x, i + 1)) {
+				let y = arr[arr.indexOf(x, i + 1)];
+				let arrTwoNamber = new Array(arr[i], y);
+				console.log(arrTwoNamber);
+				return;
+			}
 		}
+	} else {
+		console.log("Недостаточно аргументов!");
 	}
 }
 arrNamberSumm2(arrNambers, 9);
 
+//==============================================================
+
 function arrNamberSumm3(arr, namber) { // O(n)
-	let x;
-	let filter = [];
-	for (let i = 0; i < arr.length; i++) {
-		x = namber - arr[i];
-		if (filter.includes(x, 0)) {
-			let arrTwoNamber = new Array(x, arr[i]);
-			console.log(arrTwoNamber);
-			return;
-		} else {
-			filter.push(arr[i]);
+	if (arr && namber) {
+		let x;
+		let box = [];
+		for (let i = 0; i < arr.length; i++) {
+			x = namber - arr[i];
+			if (box.includes(x, 0)) {
+				let arrTwoNamber = new Array(x, arr[i]);
+				console.log(arrTwoNamber);
+				return;
+			} else {
+				box.push(arr[i]);
+			}
 		}
+	} else {
+		console.log("Недостаточно аргументов!");
 	}
 }
 arrNamberSumm3(arrNambers, 9)
 
-let arrNambers2 = [2, 3, 7, 8, 9, 17, 22, 25, 29];
+//==============================================================
 
 // Бинарный поиск  O(n log n)
+let arrNambers2 = [2, 3, 7, 8, 9, 17, 22, 25, 29];
 function arrNamberSumm4(arr, namber) {
-	for (let i = 0; i < arr.length; i++) {
-		let x = namber - arr[i];
-		let arrNew = arr.slice(i + 1);
-		while (arrNew.length >= 1) {
-			let center = Math.floor(arrNew.length / 2);
-			if (x == arrNew[center]) {
-				let arrTwoNamber = new Array(arr[i], arrNew[center]);
-				console.log("Пара чисел: " + arrTwoNamber);
-				return;
-			} else if (x < arrNew[center]) {
-				arrNew = arrNew.slice(0, center);
-			} else if (x > arr[center]) {
-				arrNew = arrNew.slice(center + 1);
+	if (arr && namber) {
+		for (let i = 0; i < arr.length; i++) {
+			let x = namber - arr[i];
+			let arrNew = arr.slice(i + 1);
+			while (arrNew.length >= 1) {
+				let center = Math.floor(arrNew.length / 2);
+				if (x == arrNew[center]) {
+					let arrTwoNamber = new Array(arr[i], arrNew[center]);
+					console.log("Пара чисел: " + arrTwoNamber);
+					return;
+				} else if (x < arrNew[center]) {
+					arrNew = arrNew.slice(0, center);
+				} else if (x > arr[center]) {
+					arrNew = arrNew.slice(center + 1);
+				}
 			}
 		}
+	} else {
+		console.log("Недостаточно аргументов!");
 	}
 	console.log("Нет такой пары чисел!");
 }
 arrNamberSumm4(arrNambers2, 11);
+
+//==============================================================
+
+let arrNambers3 = [2, 3, 7, 8, 9, 17, 22, 25, 29];
+function arrNamberSumm5(arr, namber) {
+	if (arr && namber) {
+		while (arr.length > 1) {
+			let borderL = arr[0];
+			let borderR = arr[arr.length - 1];
+			if (borderL + borderR == namber) {
+				let arrTwoNamber = new Array(borderL, borderR);
+				console.log("Два числа: " + arrTwoNamber);
+				return;
+			} else if (borderL + borderR < namber) {
+				arr = arr.slice(1);
+			} else if (borderL + borderR > namber) {
+				arr = arr.slice(0, arr.length - 1);
+			}
+		}
+		console.log("Нет такой пары чисел!");
+	} else {
+		console.log("Недостаточно аргументов!");
+	}
+}
+arrNamberSumm5(arrNambers3, 30);
