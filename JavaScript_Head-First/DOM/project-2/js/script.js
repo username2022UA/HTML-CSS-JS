609acc5af479e3a78718052df7cadaf36c9d321c
Serg <108056276+username2022UA@users.noreply.github.com>
2022-12-12 17:02:57 +0200

    new

---
+++ JavaScript_Head-First/DOM/project-2/js/script.js
@@ -0,0 +1,31 @@
+function init() {
+	var planet2 = document.getElementById("greenplanet");
+	planet2.innerHTML = "Red Alert: hit by phaser fire!";
+}
+window.onload = init;
+var access = document.getElementById("code9");
+var code = access.innerHTML;
+code = code + " midnight";
+alert(code);
+console.log(code);
+
+var planet = document.getElementById("code1");
+planet.innerHTML = "Red Alert: hit by phaser fire!";
+console.log(planet.innerHTML);
+var code1 = document.getElementById("e7");
+code1 = code1.innerHTML;
+var code2 = document.getElementById("e8");
+code2 = code2.innerHTML;
+var code3 = document.getElementById("e16");
+code3 = code3.innerHTML;
+var code4 = document.getElementById("e9");
+code4 = code4.innerHTML;
+var code5 = document.getElementById("e18");
+code5 = code5.innerHTML;
+var code6 = document.getElementById("e13");
+code6 = code6.innerHTML;
+var code7 = document.getElementById("e12");
+code7 = code7.innerHTML;
+var code8 = document.getElementById("e2");
+code8 = code8.innerHTML;
+console.log(code1 + " " + code2 + " " + code3 + " " + code4 + " " + code5 + " " + code6 + " " + code7 + " " + code8);
