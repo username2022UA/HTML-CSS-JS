3610bb60a9e2de3b841d0e176d7ddb5943085f85
Serg <108056276+username2022UA@users.noreply.github.com>
2022-12-18 19:27:37 +0200

    new

--- Tasks-JavaScript/Caesar_cipher/js/script.js
+++ Tasks-JavaScript/Caesar_cipher/js/script.js
@@ -1,16 +1,25 @@
-let text = "Hello";
+let text = "zyYvV";
 function caesar(text, key) {
 	let newText = [];
 	for (let i = 0; i < text.length; i++) {
-		let char = text.charCodeAt(i) + key;
-		newText[i] = String.fromCharCode(char)
+		let char = text.charCodeAt(i);
+		if (char >= 65 && char <= 90) {
+			char = text.charCodeAt(i) + key;
+			if (char > 90) {
+				char = char - 26;
+			}
+			newText[i] = String.fromCharCode(char);
+		} else if (char >= 97 && char <= 122) {
+			char = text.charCodeAt(i) + key;
+			if (char > 122) {
+				char = char - 26;
+			}
+			newText[i] = String.fromCharCode(char);
+		} else {
+			newText[i] = text[i];
+		}
 	}
 	newText = newText.join("");
 	console.log(newText);
-
-	//console.log(typeof newText);
-	//console.log(text.length);
 }
-//console.log(String.fromCharCode(65));
-
 caesar(text, 5);
