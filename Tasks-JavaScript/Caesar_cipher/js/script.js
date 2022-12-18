4744f29c9da330cbd66cbf2af03549b8756bc72c
Serg <108056276+username2022UA@users.noreply.github.com>
2022-12-18 20:34:27 +0200

    new

--- Tasks-JavaScript/Caesar_cipher/js/script.js
+++ Tasks-JavaScript/Caesar_cipher/js/script.js
@@ -1,4 +1,4 @@
-let text = "Hello!";
+
 function caesar(text, key) {
 	let newText = [];
 	for (let i = 0; i < text.length; i++) {
@@ -22,4 +22,31 @@ function caesar(text, key) {
 	newText = newText.join("");
 	console.log(newText);
 }
+
+function caesarOut(text, key) {
+	let newText = [];
+	for (let i = 0; i < text.length; i++) {
+		let char = text.charCodeAt(i);
+		if (char >= 65 && char <= 90) {
+			char = text.charCodeAt(i) - key;
+			if (char < 65) {
+				char = char + 26;
+			}
+			newText[i] = String.fromCharCode(char);
+		} else if (char >= 97 && char <= 122) {
+			char = text.charCodeAt(i) - key;
+			if (char < 97) {
+				char = char + 26;
+			}
+			newText[i] = String.fromCharCode(char);
+		} else {
+			newText[i] = text[i];
+		}
+	}
+	newText = newText.join("");
+	console.log(newText);
+}
+let text = "Hello! I am from America.";
+let textOut = "Mjqqt! N fr kwtr Frjwnhf.";
 caesar(text, 5);
+caesarOut(textOut, 5)
