d80c7b09896d3c0af3b8977e3a18825599c7356b
Serg <108056276+username2022UA@users.noreply.github.com>
2022-12-28 18:36:10 +0200

    new

--- Tasks-JavaScript/Caesar_cipher/js/script.js
+++ Tasks-JavaScript/Caesar_cipher/js/script.js
@@ -1,16 +1,30 @@
+const buttonInput = document.querySelector("#btnInput");
+const buttonOutput = document.querySelector("#btnOutput");
+buttonInput.addEventListener("click", function (e) {
+	let key = document.querySelector("#input").value;
+	key = Number(key);
+	let text = document.querySelector("#textarrea").value;
+	document.querySelector("#textarrea").value = caesarIn(text, key);
+});
+buttonOutput.addEventListener("click", function (e) {
+	let key = document.querySelector("#input").value;
+	key = Number(key);
+	let text = document.querySelector("#textarrea").value;
+	document.querySelector("#textarrea").value = caesarOut(text, key);
+});
 
 function caesarIn(text, key) {
 	let newText = [];
 	for (let i = 0; i < text.length; i++) {
 		let char = text.charCodeAt(i);
 		if (char >= 65 && char <= 90) {
-			char = text.charCodeAt(i) + key;
+			char = char + key;
 			if (char > 90) {
 				char = char - 26;
 			}
 			newText[i] = String.fromCharCode(char);
 		} else if (char >= 97 && char <= 122) {
-			char = text.charCodeAt(i) + key;
+			char = char + key;
 			if (char > 122) {
 				char = char - 26;
 			}
@@ -20,7 +34,7 @@ function caesarIn(text, key) {
 		}
 	}
 	newText = newText.join("");
-	console.log(newText);
+	return newText;
 }
 
 function caesarOut(text, key) {
@@ -44,23 +58,5 @@ function caesarOut(text, key) {
 		}
 	}
 	newText = newText.join("");
-	console.log(newText);
+	return newText;
 }
-//let textOut = "Mjqqt! N fr kwtr Frjwnhf.";
-//caesar(text, 5);
-//caesarOut(textOut, 5)
-const buttonIn = document.querySelector('#in');
-const buttonOut = document.querySelector('#out');
-
-document.getElementById('in').onclick = function() {
-	const text = document.getElementsByClassName('text-input').textContent;
-	alert(text);
-}
-/*document.addEventListener('click', function (event) {
-	if (event.target.closest('#in')) {
-		caesarIn(text, 5);
-	} else if ('#out') {
-		caesarOut(text, 5)
-	}
-});
-*/
